4d0c2a0e77ff094dc63474eeb42541ccc8655c58
jeanstefanovich <jeanstefanovich@gmail.com>
2017-06-13 14:17:55 +0700

    Vvod finished.

--- main.c
+++ main.c
@@ -2,10 +2,25 @@
 #include <unistd.h>
 
 int ModeChoose();
+void TrainigModul();
+
+
+typedef struct wrong{
+    struct wrong * next;
+    char[20] form1;
+    char[20] form2;
+    char[20] form3;
+    char[20] translate;
+    int lastanswer;
+} wv;
+
+
 
 int main(){
-	ModeChoose();
-	return 0;
+        int mode=ModeChoose();
+        wv *head_list;
+        TrainigModul(&head_list);
+        return 0;
 }
 
 int ModeChoose(){
@@ -16,7 +31,7 @@ int ModeChoose(){
 		" глаголов\n\n 3. Повторить 50 популярных глаголов\n"
 		"4. Повторить все неправильные глаголы\n 5. Выйти\n");
 		scanf("%d", &answer1);
-		if(answer1==1 || answer1==2 || answer1==3 || answer1==4){ 
+                if(answer1==1 || answer1==2 || answer1==3 || answer1==4 || answer1==5){
 			break;
 		}
 		else{
@@ -25,4 +40,51 @@ int ModeChoose(){
 	}
 	printf("\033[2J\033[1;1H");
     return answer1;
+}
+
+void TrainigModul(){
+    LoadWords(wv *
+}
+
+void LoadWords(wv **head_list){
+    FILE *f;
+    wv * head=*head_list, tail=&(wv *)p;
+    char st[81];
+    f=fopen("wrong_verbs.txt", "r");
+    if(f==NULL){
+                printf("\nНет доступа к файлу. ");
+                return -1;
+    }
+    while(fgets(st,81,f)){
+        int i=0, g=0;
+        p=(wv*)malloc(sizeof(wv));
+        tail->next=p;
+        tail=p;
+        while(st[i]!='\0' && st[i] && g<20){
+            p->form1[g]=st[i];
+            i++;
+            g++;
+        }
+        g=0;
+        while(st[i]!='\0' && st[i] && g<20){
+            p->form2[g]=st[i];
+            i++;
+            g++;
+        }
+        g=0;
+        while(st[i]!='\0' && st[i] && g<20){
+            p->form3[g]=st[i];
+            i++;
+            g++;
+        }
+        g=0;
+        while(st[i]!='\0' && st[i] && g<20){
+            p->translate[g]=st[i];
+            i++;
+            g++;
+        }
+    }
+    tail->next=NULL;
+    }
+    fclose(f);
 }
